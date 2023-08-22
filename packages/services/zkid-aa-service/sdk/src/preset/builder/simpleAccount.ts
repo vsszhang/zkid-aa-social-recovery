79b32d076b4a73fb2c0f0073c82fc0cac91bd6b9
vsszhang <vsszhang@gmail.com>
2023-08-23 01:55:42 +0800

    feat: add origin stackup sdk

---
+++ packages/services/zkid-aa-service/sdk/src/preset/builder/simpleAccount.ts
@@ -0,0 +1,111 @@
+import { BigNumberish, BytesLike, ethers } from "ethers";
+import { ERC4337 } from "../../constants";
+import { UserOperationBuilder } from "../../builder";
+import { BundlerJsonRpcProvider } from "../../provider";
+import {
+  EOASignature,
+  estimateUserOperationGas,
+  getGasPrice,
+} from "../middleware";
+import {
+  EntryPoint,
+  EntryPoint__factory,
+  SimpleAccountFactory,
+  SimpleAccountFactory__factory,
+  SimpleAccount as SimpleAccountImpl,
+  SimpleAccount__factory,
+} from "../../typechain";
+import { IPresetBuilderOpts, UserOperationMiddlewareFn } from "../../types";
+
+export class SimpleAccount extends UserOperationBuilder {
+  private signer: ethers.Signer;
+  private provider: ethers.providers.JsonRpcProvider;
+  private entryPoint: EntryPoint;
+  private factory: SimpleAccountFactory;
+  private initCode: string;
+  proxy: SimpleAccountImpl;
+
+  private constructor(
+    signer: ethers.Signer,
+    rpcUrl: string,
+    opts?: IPresetBuilderOpts
+  ) {
+    super();
+    this.signer = signer;
+    this.provider = new BundlerJsonRpcProvider(rpcUrl).setBundlerRpc(
+      opts?.overrideBundlerRpc
+    );
+    this.entryPoint = EntryPoint__factory.connect(
+      opts?.entryPoint || ERC4337.EntryPoint,
+      this.provider
+    );
+    this.factory = SimpleAccountFactory__factory.connect(
+      opts?.factory || ERC4337.SimpleAccount.Factory,
+      this.provider
+    );
+    this.initCode = "0x";
+    this.proxy = SimpleAccount__factory.connect(
+      ethers.constants.AddressZero,
+      this.provider
+    );
+  }
+
+  private resolveAccount: UserOperationMiddlewareFn = async (ctx) => {
+    ctx.op.nonce = await this.entryPoint.getNonce(ctx.op.sender, 0);
+    ctx.op.initCode = ctx.op.nonce.eq(0) ? this.initCode : "0x";
+  };
+
+  public static async init(
+    signer: ethers.Signer,
+    rpcUrl: string,
+    opts?: IPresetBuilderOpts
+  ): Promise<SimpleAccount> {
+    const instance = new SimpleAccount(signer, rpcUrl, opts);
+
+    try {
+      instance.initCode = await ethers.utils.hexConcat([
+        instance.factory.address,
+        instance.factory.interface.encodeFunctionData("createAccount", [
+          await instance.signer.getAddress(),
+          ethers.BigNumber.from(opts?.salt ?? 0),
+        ]),
+      ]);
+      await instance.entryPoint.callStatic.getSenderAddress(instance.initCode);
+
+      throw new Error("getSenderAddress: unexpected result");
+    } catch (error: any) {
+      const addr = error?.errorArgs?.sender;
+      if (!addr) throw error;
+
+      instance.proxy = SimpleAccount__factory.connect(addr, instance.provider);
+    }
+
+    const base = instance
+      .useDefaults({
+        sender: instance.proxy.address,
+        signature: await instance.signer.signMessage(
+          ethers.utils.arrayify(ethers.utils.keccak256("0xdead"))
+        ),
+      })
+      .useMiddleware(instance.resolveAccount)
+      .useMiddleware(getGasPrice(instance.provider));
+
+    const withPM = opts?.paymasterMiddleware
+      ? base.useMiddleware(opts.paymasterMiddleware)
+      : base.useMiddleware(estimateUserOperationGas(instance.provider));
+
+    return withPM.useMiddleware(EOASignature(instance.signer));
+  }
+
+  execute(to: string, value: BigNumberish, data: BytesLike) {
+    return this.setCallData(
+      this.proxy.interface.encodeFunctionData("execute", [to, value, data])
+    );
+  }
+
+  executeBatch(to: Array<string>, data: Array<BytesLike>) {
+    return this.setCallData(
+      this.proxy.interface.encodeFunctionData("executeBatch", [to, data])
+    );
+  }
+}
